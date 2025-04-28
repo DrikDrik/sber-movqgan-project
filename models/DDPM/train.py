6cba4c168fa27e931305c6225e910c746db677f2
DrikDrik <hasenov.ilias@gmail.com>
2025-04-28 19:32:49 +0300

    Update train.py

--- models/DDPM/train.py
+++ models/DDPM/train.py
@@ -8,7 +8,7 @@ from models.DDPM.model import generate_new_images
 from pipeline.movqgan import decode
 from pipeline.show import show_images
 
-def training_loop(ddpm, loader, n_epochs, optim, device, model, display=True, store_path="ddpm_model.pt"):
+def training_loop(ddpm, loader, n_epochs, optim, device, model, display=True, store_path="project_ddpm_best.ckpt", store_path2 = "/content/chekpoints/project_ddpm_regular.ckpt"):
     mse = nn.MSELoss()
     scheduler = lr_scheduler.ExponentialLR(optim, gamma=0.98)
     best_loss = float("inf")
@@ -26,8 +26,7 @@ def training_loop(ddpm, loader, n_epochs, optim, device, model, display=True, st
             x0 = batch.to(device)
 
             eta = torch.randn(x0.shape).to(device) 
-            # t = random.randint(0, 999)
-            t = random.randint(0, 50)
+            t = random.randint(0, 999)
 
             noisy_imgs = ddpm(x0, t, eta) 
 
@@ -56,7 +55,7 @@ def training_loop(ddpm, loader, n_epochs, optim, device, model, display=True, st
             show_images(decode(model, generate_new_images(ddpm, c=4, h=22, w=22, device=device)), f"Images generated at epoch {epoch + 1}")
 
         log_string = f"Loss at epoch {epoch + 1}: {epoch_loss:.3f}"
-        store_path2 = "/content/drive/MyDrive/model/project_ddpm_regular.ckpt"
+        
         torch.save(ddpm.state_dict(), store_path2)
 
         if best_loss > epoch_loss:
