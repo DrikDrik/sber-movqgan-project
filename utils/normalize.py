e43d38350f46567dae8cb3bfe58d1e44f2f433c8
DrikDrik <hasenov.ilias@gmail.com>
2025-04-28 19:57:29 +0300

    Create normalize.py

---
+++ utils/normalize.py
@@ -0,0 +1,19 @@
+def find_min_max(loader):
+  minn = 0
+  maxx = 0
+  for b in loader:
+    if b.min() < minn:
+      minn = b.min() # -18.6330
+    if b.max() > maxx:
+      maxx = b.max() # 19.5047
+  return minn, maxx
+
+def normalize_fn(loader):
+    minn, maxx = find_min_max(loader)
+    normalize_fn = lambda x: -1 + 2 * ((x - minn) / (maxx - minn))
+    return normalize_fn 
+
+def inverse_normalize_fn(loader):
+    minn, maxx = find_min_max(loader)
+    inverse_normalize_fn = lambda x: minn + (x + 1) * (maxx - minn) / 2
+    return inverse_normalize_fn
