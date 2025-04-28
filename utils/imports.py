838204d41b71c20ca78a25499d53aaae9a9faa72
DrikDrik <hasenov.ilias@gmail.com>
2025-04-28 16:51:27 +0300

    Create imports.py

---
+++ utils/imports.py
@@ -0,0 +1,27 @@
+import torch
+import requests
+import numpy as np
+import sys
+from PIL import Image
+from io import BytesIO
+from torch.nn.functional import mse_loss, l1_loss
+from movqgan import get_movqgan_model
+import einops
+import random
+import imageio
+from argparse import ArgumentParser
+from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
+import torch.nn as nn
+from torch.optim import Adam
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+from torchvision.transforms import Compose, ToTensor, Lambda, Resize
+import shutil
+import os
+from tqdm import tqdm
+import torch.nn.functional as F
+import torch.optim.lr_scheduler as lr_scheduler
+import torch.distributions as D
+import zipfile
