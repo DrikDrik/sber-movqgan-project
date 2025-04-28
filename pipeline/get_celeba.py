c0267cf2dcab518cc4bfc1f83884cd7d398649ec
DrikDrik <hasenov.ilias@gmail.com>
2025-04-28 17:33:54 +0300

    Update get_celeba.py

--- pipeline/get_celeba.py
+++ pipeline/get_celeba.py
@@ -1,1 +1,18 @@
+utils.download_datasets.main()
+from utils.celeba import CelebADataset as CelebA
+class CelebaCustomDataset(CelebA):
+    def __getitem__(self, idx):
+        image, _ = super().__getitem__(idx)
+        return image
 
+def get_dataset():
+  dataset = CelebaCustomDataset(
+    transform=transforms.Compose([
+      transforms.Resize(178),
+      transforms.CenterCrop(178),
+      transforms.ToTensor(),
+      lambda x: x * 2 - 1]),
+    root_dir='/content/')
+  return dataset
+  
+  
