9f659363a9bd58ba7f7036cb3dcccfdda903e605
DrikDrik <hasenov.ilias@gmail.com>
2025-04-29 13:26:38 +0300

    Update get_latent.py

--- pipeline/get_latent.py
+++ pipeline/get_latent.py
@@ -6,14 +6,7 @@ from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 
 def save_latent_representations(model, dataset, batch_size=64, save_dir='latent_representations'):
-    """
-    Сохраняет латентные представления модели в указанную папку.
-    
-    :param model: Модель, для которой сохраняются латентные представления.
-    :param dataset: Датасет для обработки
-    :param batch_size: Размер батча.
-    :param save_dir: Папка для сохранения представлений.
-    """
+
     os.makedirs(save_dir, exist_ok=True)
     model.eval()
 
@@ -24,20 +17,13 @@ def save_latent_representations(model, dataset, batch_size=64, save_dir='latent_
             batch = batch.to('cuda')
             h1_batch = model.encoder(batch)  # Обрабатываем сразу батч
 
-        # Сохраняем каждое представление в батче
         for i, h1 in enumerate(h1_batch):
             np.save(os.path.join(save_dir, f'latent_{batch_idx * batch_size + i}.npy'), h1.cpu().numpy())
-    print(f"Латентные представления сохранены в папку: {save_dir}")
 
 
-# Функция для архивации данных в ZIP
+
 def zip_dataset(input_dir, output_zip):
-    """
-    Архивирует содержимое папки в ZIP-архив.
 
-    :param input_dir: Путь к папке с датасетом.
-    :param output_zip: Имя создаваемого ZIP-архива.
-    """
     with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, _, files in os.walk(input_dir):
             for file in files:
@@ -48,29 +34,18 @@ def zip_dataset(input_dir, output_zip):
 
 class LatentDataset(Dataset):
     def __init__(self, latent_dir, transform=None):
-        """
-        Конструктор датасета.
 
-        :param latent_dir: Путь к папке с файлами .npy (латентными представлениями).
-        """
         self.latent_dir = latent_dir
         # Собираем список всех .npy файлов в папке и сортируем их
         self.latent_files = sorted([f for f in os.listdir(latent_dir) if f.endswith('.npy')])
         self.transform = transform
 
     def __len__(self):
-        """
-        Возвращает количество элементов в датасете.
-        """
+
         return len(self.latent_files)
 
     def __getitem__(self, idx):
-        """
-        Загружает латентное представление по индексу.
 
-        :param idx: Индекс элемента.
-        :return: Тензор с латентным представлением.
-        """
 
         latent_path = os.path.join(self.latent_dir, self.latent_files[idx])
         latent = np.load(latent_path)
